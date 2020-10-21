c2cf57093f45f41ae446ee1eab0a80a5c048cd89
Duc Trinh <duc.trinh.1501@gmail.com>
2020-10-21 18:24:01 +0700

    Set all fields that have "blank=True" to also have "null=True"

--- listing/models.py
+++ listing/models.py
@@ -7,12 +7,13 @@ from django.urls import reverse
 class Person(models.Model):
     last_name = models.CharField("Last Name", max_length=100, blank=True)
     first_name = models.CharField("First Name", max_length=100, blank=True)
-    middle_name = models.CharField("Middle Name", max_length=100, blank=True)
+    middle_name = models.CharField(
+        "Middle Name", max_length=100, blank=True, null=True)
 
     gender = models.CharField("Gender", blank=True, max_length=20)
 
-    birth_date = models.DateField("Birth Date", blank=True)
-    death_date = models.DateField("Death Date", blank=True)
+    birth_date = models.DateField("Birth Date", blank=True, null=True)
+    death_date = models.DateField("Death Date", blank=True, null=True)
     birth_place = models.ForeignKey(
         "Place", blank=True, null=True, related_name="birth_place", on_delete=models.CASCADE)
     death_place = models.ForeignKey(
@@ -30,21 +31,24 @@ class Person(models.Model):
                                default='au')
 
     # Works that the person has been involved with, either as author or translators
-    #related_works = models.ManyToManyField("Manuscript")
+    # related_works = models.ManyToManyField("Manuscript")
 
     def __str__(self):
-        return " ".join([self.first_name, self.middle_name, self.last_name])
+        return self.first_name + " " + self.middle_name + " " + self.last_name
 
 
 class Place(models.Model):
-    name = models.CharField("Name of Place", max_length=200, blank=True)
-    county = models.CharField("County", max_length=100, blank=True)
-    state = models.CharField("State", max_length=100, blank=True)
+    name = models.CharField(
+        "Name of Place", max_length=200, blank=True, null=True)
+    county = models.CharField("County", max_length=100, blank=True, null=True)
+    state = models.CharField("State", max_length=100, blank=True, null=True)
+
     latitude = models.CharField(
         "Latitude", max_length=100, blank=True, null=True)
     longitude = models.CharField(
         "Longitude", max_length=100, blank=True, null=True)
-    notes = models.TextField("Description Field", max_length=500, blank=True)
+    notes = models.TextField(
+        "Description Field", max_length=500, blank=True, null=True)
 
     def __str__(self):
         return self.name
