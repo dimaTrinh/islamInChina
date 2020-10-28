f84c91f470b69cd1f67b07403c71839411217d52
Duc Trinh <duc.trinh.1501@gmail.com>
2020-10-28 16:08:10 +0700

    Added 2 models: Page, Work

--- listing/models.py
+++ listing/models.py
@@ -31,10 +31,15 @@ class Person(models.Model):
                                default='au')
 
     # Works that the person has been involved with, either as author or translators
-    # related_works = models.ManyToManyField("Manuscript")
+    related_works = models.ManyToManyField("Work")
+
+    notes = models.TextField("Note Field", blank=True,
+                             null=True, max_length=100)
+    bio_notes = models.TextField(
+        "Biography Note Field", blank=True, null=True, max_length=100)
 
     def __str__(self):
-        return self.first_name + " " + self.middle_name + " " + self.last_name
+        return self.first_name + " " + self.middle_name + " " + self.last_name + " " + self.type_of
 
 
 class Place(models.Model):
@@ -52,3 +57,40 @@ class Place(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Work(models.Model):
+    arabic_title = models.CharField("Title", max_length=300, blank=True)
+    chinese_title = models.CharField("Title", max_length=300, blank=True)
+
+    author_id = models.ForeignKey(
+        "Person", blank=True, null=True, related_name="author_id", on_delete=models.CASCADE)
+
+    translator_id = models.ForeignKey(
+        "Person", blank=True, null=True, related_name="translator_id", on_delete=models.CASCADE)
+
+    author_name = models.CharField("Author", max_length=100, blank=True)
+    translator_name = models.CharField(
+        "Translator", max_length=100, blank=True)
+
+    pub_date = models.CharField("Date", max_length=50, blank=True)
+
+    TYPES = (('b', 'Book'),
+             ('m', 'Manuscript'))
+
+    type_of = models.CharField("Type of the work", max_length=1, choices=TYPES, blank=True, null=True,
+                               default='m')
+
+    summary = models.CharField("Summary", max_length=1000, blank=True)
+    num_pages = models.IntegerField("Number of Pages", blank=True, null=True)
+    transcribed = models.BooleanField("Transcribed", default=True)
+
+    def __str__(self):
+        return self.arabictitle + " " + self.chinese_title + " " + self.author_name + " " + self.translator_name
+
+
+class Page(models.Model):
+    work_id = models.ForeignKey(
+        "Work", blank=True, null=True, related_name="work_id", on_delete=models.CASCADE)
+    img_url = models.CharField("Image URL", max_length=200, blank=True)
+    transcribed = models.BooleanField("Transcribed", default=True)
